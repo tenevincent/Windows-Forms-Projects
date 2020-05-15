7d08d1bdd2f60d78006657510a18d595840d0653
vincent tene <vincent.tene@gmail.com>
2020-05-15 12:29:02 +0200

    new items added

---
+++ C#/CSharp-Practises/ConsoleDemoApp/ConsoleDemoApp/Employer.cs
@@ -0,0 +1,75 @@
+﻿namespace ConsoleDemoApp
+{
+    // Class Employer 
+    class Employer
+    {
+
+        // Fields of Employer 
+        int id;
+        string name;
+
+        // Constructor 
+        public Employer(int i, string n)
+        {
+            id = i;
+            name = n;
+        }
+
+        // Applying the custom attribute  
+        // CustomAttribute to the getId method 
+        [CustomAttribute("Accessor", count: 1)]
+        public int getId()
+        {
+            return id;
+        }
+
+        // Applying the custom attribute  
+        // CustomAttribute to the getName method 
+        [CustomAttribute("Accessor", count: 2)]
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+    // Class Employee 
+    class Employee
+    {
+
+        // Fields of Employee 
+        int id;
+        string name;
+
+        public Employee(int i, string n)
+        {
+            id = i;
+            name = n;
+        }
+
+        // Applying the custom  
+        // attribute CustomAttribute 
+        // to the getId method 
+        [CustomAttribute("Accessor", count: 3)]
+        public int getId()
+        {
+            return id;
+        }
+
+        // Applying the custom attribute  
+        // CustomAttribute to the getName method 
+        [CustomAttribute("Accessor", count: 4)]
+        public string getName()
+        {
+            return name;
+        }
+
+       public (string name, string vorname) GetEmployeeProperties()
+        {
+
+
+            return ("Vincent Tene", "Kengne Paul");
+
+        }
+
+    }
+}
---
+++ C#/CSharp-Practises/ConsoleDemoApp/ConsoleDemoApp/MyAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemoApp
+{
+    // C# program to illustrate the 
+    // use of custom attributes 
+    using System;
+
+    // Creating Custom attribute MyAttribute 
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class MyAttribute : Attribute
+    {
+
+        // Provides name of the member 
+        private string name;
+
+        // Provides description of the member 
+        private string action;
+
+        // Constructor 
+        public MyAttribute(string name, string action)
+        {
+            this.name = name;
+            this.action = action;
+        }
+
+        // property to get name 
+        public string Name
+        {
+            get { return name; }
+        }
+
+        // property to get description 
+        public string Action
+        {
+            get { return action; }
+        }
+    }
+
+
+
+}
